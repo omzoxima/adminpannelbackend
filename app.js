92cc17a36200b92e37b349b09dfa248ca0c2e0e7
omzoxima <om.prakash@zoxima.com>
2025-07-18 00:45:52 +0530

    apply middleware

--- app.js
+++ app.js
@@ -10,10 +10,23 @@ const episodeRoutes = require('./routes/episode');
 const userRoutes = require('./routes/user');
 const bodyParser = require('body-parser');
 const { sequelize } = require('./models/index.js');
+const { authenticateToken } = require('./utils/jwt');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// JWT middleware for all /api routes except /api/admin/login
+app.use((req, res, next) => {
+  if (req.path === '/api/admin/login') {
+    return next();
+  }
+  // Only protect /api/* routes
+  if (req.path.startsWith('/api/')) {
+    return authenticateToken(req, res, next);
+  }
+  next();
+});
 
 // Mount admin routes
 app.use('/api/admin', adminRoutes);
