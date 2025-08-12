7cee40253fe6c7f204b1523bf32f3810b2fce427
omzoxima <om.prakash@zoxima.com>
2025-08-12 19:06:14 +0530

    add crousel issue

--- routes/series.js
+++ routes/series.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllSeries, getSeriesById, createSeries, updateSeriesStatus } from '../controllers/seriesController.js';
+import { getAllSeries, getSeriesById, createSeries, updateSeriesStatus, updateSeries } from '../controllers/seriesController.js';
 import multer from 'multer';
 
 const router = express.Router();
@@ -7,7 +7,14 @@ const upload = multer({ storage: multer.memoryStorage(), limits: { fileSize: 500
 
 router.get('/', getAllSeries);
 router.get('/:id', getSeriesById);
-router.post('/create', upload.single('thumbnail'), createSeries);
+router.post('/create', upload.fields([
+  { name: 'thumbnail', maxCount: 1 },
+  { name: 'carousel_image', maxCount: 1 }
+]), createSeries);
+router.put('/update', upload.fields([
+  { name: 'thumbnail', maxCount: 1 },
+  { name: 'carousel_image', maxCount: 1 }
+]), updateSeries);
 router.post('/update-status', updateSeriesStatus);
 
 export default router; 
